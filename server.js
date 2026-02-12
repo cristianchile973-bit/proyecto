819a3b8dbfeffbb0266972fd34944f168868ec20
cristianchile973-bit <cristianchile973@gmail.com>
2026-02-12 16:25:47 -0600

    Update nodemailer transport configuration

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.post('/guardar-respuesta', async (req, res) => {
     try {
         let transporter = nodemailer.createTransport({
       service: 'smtp.gmail.com',
+           port: 587,
+         secure: false,
        auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
