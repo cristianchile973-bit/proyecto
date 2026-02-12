a0fcbecf4987873d2aeeab9f5b993f6f3ab21f59
cristianchile973-bit <cristianchile973@gmail.com>
2026-02-12 16:06:18 -0600

    Update Gmail service configuration for nodemailer

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/guardar-respuesta', async (req, res) => {
 
     try {
         let transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: 'smtp.gmail.com',
        auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
